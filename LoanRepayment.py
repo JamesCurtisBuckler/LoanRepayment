ee53d4fdf4998fdc15672e79bcc065ced3017641
JamesCurtisBuckler <136401472+JamesCurtisBuckler@users.noreply.github.com>
2023-07-10 15:51:57 -0700

    Add files via upload

---
+++ LoanRepayment.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 15:45:21 2023
+
+@author: James Curtis Buckler
+"""
+
+# -*- coding: utf-8 -*-
+def calculate_monthly_repayment(PV, r, n):
+    # Convert APR to monthly interest rate
+    r = r / 100 / 12
+    
+    # Calculate the denominator term (1 - (1+r)^-n)
+    denominator = 1 - (1 + r) ** -n
+    
+    # Calculate the monthly repayment amount
+    Pmt = r * PV / denominator
+    
+    return Pmt
+
+# Get user input for PV, r, and n
+PV = float(input("Enter the loan amount (Present Value): $"))
+r = float(input("Enter the annual interest rate (APR): "))
+n = int(input("Enter the number of months: "))
+
+# Call the function to calculate the monthly repayment amount
+monthly_repayment = calculate_monthly_repayment(PV, r, n)
+print("Monthly Repayment Amount: $", round(monthly_repayment, 2))
